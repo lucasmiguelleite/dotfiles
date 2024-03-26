7bb22b1723ba6510538f971bb7f8330c32a31cce
Lucas <lucasleite.miguel10@gmail.com>
2024-03-26 20:46:20 -0300

    Vim settings file
    config.lua - arquivo do lunarvim
    setting.json - arquivo da extensao do vim no vscode

---
+++ config.lua
@@ -0,0 +1,38 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Remaps --------------------------------------------------------------------------------------
+-- Adding an empty line below, above and below with insert
+lvim.keys.normal_mode["op"] = "o<Esc>k"
+lvim.keys.normal_mode["oi"] = "O<Esc>j"
+lvim.keys.normal_mode["oo"] = "A<CR>"
+
+-- Create a tab
+lvim.keys.normal_mode["te"] = ":tabe<CR>"
+
+-- Navigate between buffers
+lvim.keys.normal_mode["ty"] = ":bn<CR>"
+lvim.keys.normal_mode["tr"] = ":bp<CR>"
+
+-- Delete a buffer
+lvim.keys.normal_mode["td"] = ":bd<CR>"
+
+-- Create splits
+lvim.keys.normal_mode["th"] = ":split<CR>"
+lvim.keys.normal_mode["tv"] = ":vsplit<CR>"
+
+-- Close splits and others
+lvim.keys.normal_mode["tt"] = ":q<CR>"
+
+-- Call command shortcut
+lvim.keys.normal_mode["tc"] = ":!"
+
+-- Change <Esc> to jj
+lvim.keys.insert_mode["jj"] = "<Esc>"
+
+
+
+vim.opt.timeoutlen = 500
