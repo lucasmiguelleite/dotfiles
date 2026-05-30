af7872ee2e244d698558a01651a459cf5c8775f5
Lucas Miguel Leite <lucasleite.miguel10@gmail.com>
2026-05-30 11:45:46 -0300

    Replace exa with eza

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew install git zoxide atuin exa bat mise \
+brew install git zoxide atuin eza bat mise \
   zsh-history-substring-search zsh-autosuggestions \
   powerlevel10k neovim
 
